3707fbccdf8d75a06398ae5d8802855ccfba1eeb
Alan Lawson <alan.lawson.glasgow@gmail.com>
2020-07-19 23:12:29 +0100

    4.7.3 binaries installer completed

--- Docs/advanced/general_classes/annotated.js
+++
@@ -1,7 +0,0 @@
-var annotated =
-[
-    [ "bncs_disklog", "classbncs__disklog.html", "classbncs__disklog" ],
-    [ "cBncsVersion", "classc_bncs_version.html", "classc_bncs_version" ],
-    [ "decodeBNCSString", "classdecode_b_n_c_s_string.html", "classdecode_b_n_c_s_string" ],
-    [ "decodeCaptionString", "classdecode_caption_string.html", "classdecode_caption_string" ]
-];
--- Docs/advanced/general_classes/classdecode_b_n_c_s_string.js
+++
@@ -1,8 +0,0 @@
-var classdecode_b_n_c_s_string =
-[
-    [ "decodeBNCSString", "classdecode_b_n_c_s_string.html#ac5c5044861eb049933928bb4a6206678", null ],
-    [ "decodeBNCSString", "classdecode_b_n_c_s_string.html#a4ffb51dfda742b5bfc2b3fb085325f40", null ],
-    [ "~decodeBNCSString", "classdecode_b_n_c_s_string.html#a68b7db69726b42dfee15aeae34ea7f27", null ],
-    [ "sub", "classdecode_b_n_c_s_string.html#a6d7b6ad5ffb5aa6880595ce02518e65a", null ],
-    [ "subs", "classdecode_b_n_c_s_string.html#a9f81063719c160128d7175dbff328db5", null ]
-];
